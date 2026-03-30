b4e86013f65ffb22f10bc22040b3f8d772c7ebf0
Diego <diegokemer26gmail.com>
2026-03-30 18:27:20 -0300

    Renderizado de rutas

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'res/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'res/:slug/**',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'user-panel/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'user-panel/:id/**',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
